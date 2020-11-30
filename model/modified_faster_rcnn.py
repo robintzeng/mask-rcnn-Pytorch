ba41993af477a3e5fea5d312ce66cfe2a75c9f06
robin <robintzeng@gmail.com>
2020-11-30 11:35:51 +0800

    add calculate param

--- model/modified_faster_rcnn.py
+++ model/modified_faster_rcnn.py
@@ -9,24 +9,32 @@ from torchvision.models.detection.mask_rcnn import MaskRCNNPredictor
 from torchvision.models.detection.rpn import AnchorGenerator
 from .IA_faster_rcnn import FasterRCNNIA
 import timm
-from model.backbone import TimmToVisionFPN, TimmToVision, resnet50_fpn
+from model.backbone import TimmToVisionFPN, TimmToVision, resnet50_fpn, calculate_param
 # connect our models here !!
 
 
 def get_model(num_classes):
     # load an instance segmentation model pre-trained on COCO
-    ## without FPN
-    m = timm.create_model('cspresnet50', pretrained=True, num_classes=0, global_pool='')
-    backbone = TimmToVision(m,1024)
-    
-    ## FPN official version
+    # without FPN
+    # m = timm.create_model('cspresnet50', pretrained=True, num_classes=0, global_pool='')
+    # backbone = TimmToVision(m, 1024)
+
+    m = timm.create_model('cspresnet50', features_only=True, pretrained=True)
+    backbone = TimmToVisionFPN(m)
+
+    # FPN official version
     #backbone = resnet50_fpn()
-    
+
     # ["0"] rather than [0]
     # roi_pooler = torchvision.ops.MultiScaleRoIAlign(featmap_names=['0', '1', '2', '3'],
     #                                                output_size=7,
     #                                                sampling_ratio=2)
 
-    model = FasterRCNNIA(backbone,num_classes=num_classes)
+    model = FasterRCNNIA(backbone, num_classes=num_classes)
 
     return model
+
+
+if __name__ == "__main__":
+    m = get_model(20)
+    print(calculate_param(m))
--- model/modified_mask_rcnn.py
+++ model/modified_mask_rcnn.py
@@ -5,7 +5,7 @@ import torch.utils.data
 from PIL import Image
 import torchvision
 from torchvision.models.detection import roi_heads
-from torchvision.models.detection.faster_rcnn import FastRCNNPredictor,TwoMLPHead
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor, TwoMLPHead
 from torchvision.models.detection.mask_rcnn import MaskRCNNPredictor
 from torchvision.models.detection.rpn import AnchorGenerator
 from torchvision.models.detection import MaskRCNN, FasterRCNN
@@ -19,19 +19,18 @@ from .IA_mask_rcnn import MaskRCNNIA
 
 def get_model(num_classes):
     # load an instance segmentation model pre-trained on COCO
-    
+
     # Without FPN cannot be used with MaskRCNNIA because of the version of the torchvision
     m = timm.create_model('cspresnet50', pretrained=True, num_classes=0, global_pool='')
-    backbone = TimmToVision(m,1024)
-    
-    ## with FPN 
+    backbone = TimmToVision(m, 1024)
+
+    # with FPN
     #m = timm.create_model('cspresnet50', features_only=True, pretrained=True)
     #backbone = TimmToVisionFPN(m)
-    
-    ## FPN official version
+
+    # FPN official version
     #backbone = resnet50_fpn()
-    
+
     model = MaskRCNNIA(backbone, num_classes)
 
     return model
-
